d0cc78826d89274569287f49a6495ec69a6728d3
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-06-11 20:43:07 +0200

    Notes are added to Core Data

--- To Do App/FirstViewController.swift
+++ To Do App/FirstViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-var toDoList = [String]()
+var toDoList = [NSManagedObject]()
 
 class FirstViewController: UIViewController, UITableViewDelegate {
     
@@ -24,15 +24,14 @@ class FirstViewController: UIViewController, UITableViewDelegate {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        
         return toDoList.count
-        
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = toDoList[indexPath.row]
-        //cell.detailTextLabel?.text = "Ho ho"
+        let note = toDoList[indexPath.row]
+        cell.textLabel?.text = note.valueForKey("note") as! String
+        // set image for cell
         var image: UIImage = UIImage(named: "AppIcon")!
         cell.imageView?.image = image
         return cell
@@ -44,31 +43,41 @@ class FirstViewController: UIViewController, UITableViewDelegate {
     
     internal func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            let note = toDoList[indexPath.row]
+            let noteText = note.valueForKey("note") as! String
+            removeFromNotes(noteText)
             toDoList.removeAtIndex(indexPath.row)
-            //NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
             toDoListTable.reloadData()
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        // create contex of our App
+        let appDell: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let contextOfOurApp: NSManagedObjectContext = appDell.managedObjectContext
+        
+        // create request for data in Core Data entity - with this we get all of our data
+        var request = NSFetchRequest(entityName: "Notes")
+        
+        do {
+            // try to get data from Corde Data entity
+            var results = try contextOfOurApp.executeFetchRequest(request)
+            
+            // check if there is any data
+            if results.count > 0 {
+                toDoList = results as! [NSManagedObject]
+            }    
+        } catch let error as NSError{
+            print ("There was an error \(error), \(error.userInfo)")
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let appDell: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        let contextOfOurApp: NSManagedObjectContext = appDell.managedObjectContext
-        
-        var newNote = NSEntityDescription.insertNewObjectForEntityForName("Notes", inManagedObjectContext: contextOfOurApp)
-        
-        newNote.setValue(1, forKey: "note_id")
-        newNote.setValue("Nowa notatka", forKey: "note")
-        
-        do {
-            try contextOfOurApp.save()
-            
-        } catch {
-            //TODO add popup error
-        }
-        
     }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -76,9 +85,7 @@ class FirstViewController: UIViewController, UITableViewDelegate {
     }
     
     override func viewDidAppear(animated: Bool) {
-        
         toDoListTable.reloadData()
-        
     }
     
     func promptForAnswer() {
@@ -86,16 +93,68 @@ class FirstViewController: UIViewController, UITableViewDelegate {
         ac.addTextFieldWithConfigurationHandler(nil)
         
         let submitAction = UIAlertAction(title: "Add", style: .Default) { [unowned self, ac] (action: UIAlertAction!) in
-            let answer = ac.textFields![0] //as! UITextField
+            let answer = ac.textFields![0]
+            
             // add answear to toDoList Array
-            toDoList.append(answer.text!)
+            self.saveNote(answer.text!)
             self.toDoListTable.reloadData()
 
         }
         
         ac.addAction(submitAction)
-        
         presentViewController(ac, animated: true, completion: nil)
     }
     
+    func saveNote(note: String) {
+        
+        // create context for our app
+        let appDell: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let contextOfOurApp: NSManagedObjectContext = appDell.managedObjectContext
+        
+        // create Core Data entity
+        var newNote = NSEntityDescription.insertNewObjectForEntityForName("Notes", inManagedObjectContext: contextOfOurApp)
+        
+        newNote.setValue(note, forKey: "note")
+        
+            do {
+                try contextOfOurApp.save()
+                toDoList.append(newNote)
+        
+            } catch let error as NSError{
+                print ("There was an error \(error), \(error.userInfo)")
+            }
+    }
+    
+    func removeFromNotes(noteText: String) {
+        
+        // create context for our app
+        let appDell: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let contextOfOurApp: NSManagedObjectContext = appDell.managedObjectContext
+        
+        // create request for data in Core Data entity - with this we get all of our data
+        var request = NSFetchRequest(entityName: "Notes")
+        request.predicate = NSPredicate(format: "note == %@", noteText)
+        
+        // we need to use if we want to see actual data in our app
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try contextOfOurApp.executeFetchRequest(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    contextOfOurApp.deleteObject(result)
+                    
+                    // we save our data
+                    do {
+                        try contextOfOurApp.save()
+                    } catch let error as NSError{
+                        print ("There was an error \(error), \(error.userInfo)")
+                    }
+                }
+            }
+            
+        } catch let error as NSError {
+            print ("There was an error \(error), \(error.userInfo)")
+        }
+    }
 }
