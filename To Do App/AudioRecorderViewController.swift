e49085faaef929ce5c90d57ebf78e9b895ea5b3b
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-06-17 00:03:19 +0200

    Basic setup for AudioRecorderController

--- To Do App/AudioRecorderViewController.swift
+++ To Do App/AudioRecorderViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import AVFoundation
 
-class AudioRecorderViewController: UIViewController {
+class AudioRecorderViewController: UIViewController, AVAudioRecorderDelegate {
     
+    var audioRecorder: AVAudioRecorder!
+    
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var stopRecordingButton: UIButton!
     
     @IBAction func recordAudio(sender: AnyObject) {
+        recordButton.enabled = false
+        stopRecordingButton.enabled = true
+        
+        createRecordingSession()
+        getRecordingTime()
+
     }
 
     override func viewDidLoad() {
@@ -23,6 +34,38 @@ class AudioRecorderViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        stopRecordingButton.enabled = false
+    }
+    
+    func createRecordingSession() {
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        try! audioRecorder = AVAudioRecorder(URL: createRecordFileURL("Some name"), settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func createRecordFileURL(fileName: String) -> NSURL {
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,.UserDomainMask, true)[0] as String
+        let recordingName = fileName
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        return filePath!
+    }
+    
+    func getRecordingTime() -> String {
+        let currentDate = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale.currentLocale()
+        dateFormatter.dateFormat = " MMM dd, yyyy, HH:mm:ss"
+        let convertedDate = dateFormatter.stringFromDate(currentDate)
+        return convertedDate
     }
     
 
