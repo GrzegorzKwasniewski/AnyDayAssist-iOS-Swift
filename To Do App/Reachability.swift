b119251dd4ac3c26719064b124745e7edd91a4cd
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2016-07-25 19:44:10 +0200

    added checking for internet connection ,unwrapped player?

---
+++ To Do App/Reachability.swift
@@ -0,0 +1,9 @@
+//
+//  Reachability.swift
+//  To Do App
+//
+//  Created by Grzegorz Kwaśniewski on 25/07/16.
+//  Copyright © 2016 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import Foundation
