c60ce6ff3c82dd60c6260f37165a32a863abe302
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-01-14 01:23:41 +0100

    Add helper methods in UIView extension for setting constraints

---
+++ To Do App/UIViewEx.swift
@@ -0,0 +1,57 @@
+//
+//  UIViewEx.swift
+//  AnyDay
+//
+//  Created by Grzegorz Kwaśniewski on 12/01/17.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
+        
+        _ = anchor(top, left: left, bottom: bottom, right: right, topConstant: topConstant, leftConstant: leftConstant, bottomConstant: bottomConstant, rightConstant: rightConstant)
+    }
+    
+    func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0, widthConstant: CGFloat = 0, heightConstant: CGFloat = 0) -> [NSLayoutConstraint] {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchors = [NSLayoutConstraint]()
+        
+        if let top = top {
+            anchors.append(topAnchor.constraintEqualToAnchor(top, constant: topConstant))
+        }
+        
+        if let left = left {
+            anchors.append(leftAnchor.constraintEqualToAnchor(left, constant: leftConstant))
+        }
+        
+        if let bottom = bottom {
+            anchors.append(bottomAnchor.constraintEqualToAnchor(bottom, constant: -bottomConstant))
+        }
+        
+        if let right = right {
+            anchors.append(rightAnchor.constraintEqualToAnchor(right, constant: -rightConstant))
+        }
+        
+        if widthConstant > 0 {
+            anchors.append(widthAnchor.constraintEqualToConstant(widthConstant))
+        }
+        
+        if heightConstant > 0 {
+            anchors.append(heightAnchor.constraintEqualToConstant(heightConstant))
+        }
+        
+        anchors.forEach({$0.active = true})
+        
+        return anchors
+    }
+}
