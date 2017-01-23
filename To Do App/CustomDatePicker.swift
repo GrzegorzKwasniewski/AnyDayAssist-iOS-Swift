0a0aef7cfb3acaabf333e60cb2835b056428f690
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-01-23 18:09:13 +0100

    Limit Date Picker Min Date
    * user can only set remainder for text note for future date

--- To Do App/CustomDatePicker.swift
+++ To Do App/CustomDatePicker.swift
@@ -12,14 +12,23 @@ class CustomDatePicker: UIDatePicker {
     
     // MARK: - Initializers
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func drawRect(rect: CGRect) {
         super.drawRect(rect)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         self.commonInit()
     }
     
     // MARK: - Custom Functions
     
     func commonInit() {
+        self.minimumDate = NSDate()
     }
     
     // MARK: - Layout Functions
