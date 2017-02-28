b7a259abc06d9fa339351b069ca931a9be95169d
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-02-28 17:11:18 +0100

    Add Class For Testing TextNotesDatasource

--- AnyDayTests/TextNotesDatasourceTest.swift
+++ AnyDayTests/TextNotesDatasourceTest.swift
@@ -8,28 +8,26 @@
 
 import XCTest
 
-class e: XCTestCase {
+@testable import AnyDay
+class TextNotesDatasourceTest: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_NumberOfSectionsIsOne() {
+        
+        let tableViewDelegate = TextNotesTableDelegate()
+        let tableView = UITableView()
+        
+        tableView.delegate = tableViewDelegate
+        
+        let numberOfSections = tableView.numberOfSections
+        XCTAssertEqual(numberOfSections, 1)
+        
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
