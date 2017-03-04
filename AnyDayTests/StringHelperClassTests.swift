4d6f8db5b9e5d3196978eadde95d8df9b85ca7a4
Grzegorz Kwaśniewski <grzegorz.kwasniewski2@gmail.com>
2017-03-04 22:22:07 +0100

    Add Class For Testing StringHelperClass

---
+++ AnyDayTests/StringHelperClassTests.swift
@@ -0,0 +1,22 @@
+//
+//  StringHelperClassTests.swift
+//  AnyDay
+//
+//  Created by Grzegorz Kwaśniewski on 04/03/17.
+//  Copyright © 2017 Grzegorz Kwaśniewski. All rights reserved.
+//
+
+import XCTest
+
+class StringHelperClassTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+}
